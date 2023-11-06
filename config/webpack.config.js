599966d164d40e2e0a5d88e324924ddc41df8c26
HGZ233 <1405580860@qq.com>
2023-11-06 17:45:04 +0800

    【结构】【定义】【构建】项目构建

---
+++ config/webpack.config.js
@@ -0,0 +1,12 @@
+// webpack.config.js
+const path = require('path')
+
+module.exports = {
+  target: 'web', // 默认打包成web平台的
+  mode: 'production', // 环境 development 和 production 环境 链接： https://www.webpackjs.com/concepts/mode/#mode-development
+  entry: path.resolve(__dirname, '../src/index.jsx'), // 文件的入口
+  output: {
+    filename: 'js/[name].[chunkhash:8].js', // 文件名
+    path: path.resolve(__dirname, '../dist') // 文件输出地址
+  }
+}
